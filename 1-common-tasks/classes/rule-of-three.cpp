255f65fca7003d793130590b4ebbac3f32621ab4
Joseph Mansfield <sftrabbit@gmail.com>
2015-04-03 16:28:42 +0100

    Rewrite the rule of three sample

---
+++ 1-common-tasks/classes/rule-of-three.cpp
@@ -0,0 +1,54 @@
+// The rule of three
+
+#include <utility>
+
+class foo
+{
+	public:
+		foo()
+			: p{new int{5}}
+		{ }
+
+		foo(const foo& other)
+			: p{new int{*(other.p)}}
+		{ }
+
+		foo& operator=(foo other)
+		{
+			std::swap(p, other.p);
+			return *this;
+		}
+
+		~foo()
+		{
+			delete p;
+		}
+
+	private:
+		int* p;
+};
+
+// Encapsulate and safely manage dynamically allocated resources to
+// avoid memory leaks.
+// 
+// The class `foo` dynamically allocates an `int` object in its
+// constructor ([9]). To ensure that the lifetime of this `int` is
+// tied to the lifetime of the `foo` object that allocated it, 
+// we deallocate it in `foo`'s destructor ([24]). This idiom, known as
+// [RAII](http://en.wikipedia.org/wiki/Resource_Acquisition_Is_Initialization),
+// ensures that dynamically allocated objects are properly destroyed
+// and no memory is leaked, even if an exception occurs.
+// 
+// We also implement a copy constructor ([12-14]) and an assignment
+// operator ([16-20]) to manage this resource when copying or
+// assigning a `foo`. In general, if a class has a destructor, it
+// almost certainly needs these functions too. This idea is known
+// as the *rule of three*.
+// 
+// We can typically avoid manual memory management entirely by using
+// existing RAII types.
+
+int main()
+{
+	foo f;
+}

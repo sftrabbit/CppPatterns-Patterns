d97275d4ad8d0c71e4aa0ba9ce6fc907e2da2b4c
Joseph Mansfield <sftrabbit@gmail.com>
2015-04-03 22:20:32 +0100

    Clarify unique ownership sample

--- 1-common-tasks/memory-management/unique-ownership.cpp
+++ 1-common-tasks/memory-management/unique-ownership.cpp
@@ -10,8 +10,8 @@ void pass_unique_ownership(std::unique_ptr<foo> obj)
 
 int main()
 {
-	std::unique_ptr<foo> obj1 = std::make_unique<foo>();
-	pass_unique_ownership(std::move(obj1));
+	std::unique_ptr<foo> obj = std::make_unique<foo>();
+	pass_unique_ownership(std::move(obj));
 }
 
 // Transfer unique ownership of a dynamically allocated object to
@@ -21,8 +21,10 @@ int main()
 // which has ownership of a dynamically allocated `foo` object
 // (created with the [`std::make_unique`](cpp/memory/unique_ptr/make_unique)
 // utility function). [!14] then demonstrates passing ownership of this
-// object to a function. The call to [`std::move`](cpp/utility/move)
-// allows the `std::unique_ptr` to be moved into the function.
+// object to the function `func`. After passing ownership, `main` no
+// longer has access to the `foo` object. The call to
+// [`std::move`](cpp/utility/move) is required to allow the
+// `std::unique_ptr` to be moved into the function.
 // 
 // In other cases, you may instead wish to
 // [share ownership of an object](/common-tasks/memory-management/share-ownership-of-object.html).

ef306a7de0d09aa8b22d1b3ff3ab6b95fd6059cd
Joseph Mansfield <sftrabbit@gmail.com>
2015-04-05 10:36:35 +0100

    Add random element sample

---
+++ 1-common-tasks/random/choose-random-element.cpp
@@ -0,0 +1,31 @@
+// Choose a random element
+ 
+#include <random>
+#include <vector>
+
+int main()
+{
+	std::vector<int> v = {10, 15, 20, 25, 30};
+
+	std::random_device random_device;
+	std::mt19937 engine{random_device()};
+	std::uniform_int_distribution<int> dist(0, v.size() - 1);
+	
+	int random_element = v[dist(engine)];
+}
+
+// Choose a random element from a container.
+// 
+// On [8], we create a [`std::vector`](cpp/container/vector) from
+// which we want to select a random element.
+// 
+// We then effectively [roll a die](/common-tasks/random/roll-a-die.html)
+// where the numbers on the die are the indices of elements in the
+// container. That is, we seed the
+// [`std::mt19937`](cpp/numeric/random/mersenne_twister_engine) on
+// [10-11] and create a uniform random distribution of integers from
+// `0` to `v.size() - 1` inclusive on [12]. This distribution will
+// give us any integer in this range with equal probability.
+// 
+// On [14], we call `dist(engine)` to generate the random index, and
+// use this value to access an element from `v`.

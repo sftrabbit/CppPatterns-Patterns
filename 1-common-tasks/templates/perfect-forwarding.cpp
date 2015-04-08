5c89374cb591efba55e769c940672a91c1f80aff
Joseph Mansfield <sftrabbit@gmail.com>
2015-04-08 19:01:15 +0100

    Edit perfect forwarding sample

--- 1-common-tasks/templates/perfect-forwarding.cpp
+++ 1-common-tasks/templates/perfect-forwarding.cpp
@@ -1,48 +1,38 @@
 // Perfect forwarding
 
-#include <iostream>
-#include <string>
 #include <utility>
 
-static int pair_counter = 0;
-
 template<typename T, typename U>
-auto
-make_pair_and_count(T&& t, U&& u)
+std::pair<T, U> make_pair_wrapper(T&& t, U&& u)
 {
-   pair_counter++;
-   return std::make_pair(std::forward<T>(t), std::forward<U>(u));
+	return std::make_pair(std::forward<T>(t),
+	                      std::forward<U>(u));
 }
 
-// Forwards an argument of a wrapping function to a wrapped function
-// *as if* the wrapped function was called directly without the wrapper.
+// Forward arguments of one function to another as though the wrapped
+// function had been called directly.
 // 
-// Perfect forwarding preserves argument's value category (rvalue/lvalue) and
-// const/volatile modifiers. Perfect forwarding is done in two steps:
-// receive a *forwarding reference* (also known as *universal reference*),
-// then forward it using `std::forward`(cpp/utility/forward). 
-// In our example `t` and `u` on [14] are forwarding references.
+// Perfect forwarding allows us to preserve an argument's value
+// category (lvalue/rvalue) and `const`/`volatile` modifiers. Perfect
+// forwarding is performed in two steps: receive a *forwarding
+// reference* (also known as *universal reference*), then forward it
+// using [`std::forward`](cpp/utility/forward).
+// 
+// In our example, the arguments `t` and `u` on [6] are forwarding
+// references because they are declared in the form `X&&` where `X`
+// is a template parameter. We use `std::forward` on [8-9] to forward
+// these arguments to [`std::make_pair`](cpp/utility/pair/make_pair),
+// allowing them to be moved into the pair when the original argument
+// was an rvalue expression.
 //
-// [`std::make_pair`](cpp/utility/pair/make_pair) is itself 
-// declared with forwarding references as arguments, so that you can
-// *move-in* rvalues into a pair without any extra temporaries, and
-// our wrapping function preserves this behavior.
-//
-// Perfect forwarding is often done with [variadic templates](cpp/language/parameter_pack) to wrap
-// calls to functions with arbitrary number of arguments. For example,
-// [`std::make_unique`](cpp/memory/unique_ptr/make_unique) and
-// [`std::make_shared`](cpp/memory/shared_ptr/make_shared) are 
-// perfect forwarding their arguments to the constructor of the 
-// wrapped type.
+// Perfect forwarding is often used with [variadic templates](cpp/language/parameter_pack)
+// to wrap calls to functions with an arbitrary number of arguments.
+// For example, [`std::make_unique`](cpp/memory/unique_ptr/make_unique)
+// and [`std::make_shared`](cpp/memory/shared_ptr/make_shared) both
+// use perfect forwarding to forward their arguments to the
+// constructor of the wrapped type.
 
 int main()
 {
-   using namespace std;
-
-   pair<int, int> p1{ make_pair_and_count(1, 2) };
-
-   const string str("str");
-   pair<int, string> p2{ make_pair_and_count(p1.first, str) };
-
-   cout << pair_counter << " pairs were made";
+	std::pair<int, int> p1{ make_pair_wrapper(1, 2) };
 }

fefc77c1b3c02441951ff318209cd0c16e4df78e
Joseph Mansfield <sftrabbit@gmail.com>
2015-04-10 22:21:41 +0100

    Review and edit weak reference sample

--- 1-common-tasks/memory-management/weak-reference.cpp
+++ 1-common-tasks/memory-management/weak-reference.cpp
@@ -1,71 +1,75 @@
-// Weak Reference
+// Weak reference
 
 #include <memory>
 
-class foo;
-
-class bar
-{
-public:
-	void set_foo(const std::shared_ptr<foo>& foo)
-	{
-		this->sp = foo;
-	}
-
-	void notify_ready() {}
-
-private:
-	std::shared_ptr<foo> sp;
-};
+class bar;
 
 class foo
 {
 public:
-	void set_bar(const std::weak_ptr<bar>& bar)
+	foo(const std::shared_ptr<bar>& b)
 	{
-		this->wp = bar;
-	}
-
-	void start()
-	{
-		std::shared_ptr<bar> sp = this->wp.lock();
-		if (sp)
-			sp->notify_ready();
+		this->forward_reference = b;
 	}
 
 private:
-	std::weak_ptr<bar> wp;
+	std::shared_ptr<bar> forward_reference;
 };
 
-// Maintain a non-owning (weak) reference to a dynamically allocated object
-// managed by a [`std::shared_ptr`](cpp/memory/shared_ptr) to avoid a circular
-// dependency.
+class bar
+{
+public:
+	void set_back_reference(const std::weak_ptr<foo>& f)
+	{
+		this->back_reference = f;
+	}
+
+	void do_something()
+	{
+		std::shared_ptr<foo> shared_back_reference = this->back_reference.lock();
+		if (shared_back_reference) {
+			// Use *shared_back_reference
+		}
+	}
+
+private:
+	std::weak_ptr<foo> back_reference;
+};
+
+// Maintain a non-owning reference to a shared dynamically allocated
+// object to break circular dependencies.
 //
-// Weak pointers are non-owning references. As they do not contribute to the
-// reference count of the managed object they refer to, the object can be
-// deleted by another unit of code at any time. However, a
+// The [`std::weak_ptr`](cpp/memory/weak_ptr) type represents
+// a non-owning reference to dynamically allocated object with
+// shared ownership ([`std::shared_ptr`](cpp/memory/shared_ptr)). As
+// they do not contribute to the reference count of the managed object
+// they refer to, the object can be destroyed at any time when all
+// `std::shared_ptr`s give up ownership. However, a
 // [`std::weak_ptr`](cpp/memory/weak_ptr) can be converted to a
 // [`std::shared_ptr`] to provide temporary ownership and safe access to the
 // object.
 //
-// In this example a bar object may depend on a foo object and should be
-// notified when the foo is ready. However bar's lifetime is independent of
-// foo's. If foo stores a plain reference to bar, it risks refering to a deleted
-// object when it attempts to notify bar. If foo stores a [`std::shared_ptr`] to
-// bar, it introduces a circular dependency, unnecessarily preventing foo from
-// being deleted. The solution is to use a [`std::weak_ptr`]. When foo is
-// started, it checks if bar still exists by attempting to take temporary
-// ownership and then notifies bar that it is ready (29-31).
+// In this sample, we have two classes: `foo` on [7-17], and `bar` on
+// [19-33]. A `foo` object has shared ownership of a `bar` ([16]), and
+// `bar` requires some form of reference back to the `foo` that owns it.
+// If this back reference were a `std::shared_ptr<foo>`, it would introduce
+// a circular dependency, making it impossible for either object to be
+// destroyed. If it were a normal reference type (`foo&`), it risks
+// refering to a deleted object when it attempts to use it, as the
+// lifetime of `foo` is independent of `bar`.
+// 
+// The solution is to use a `std::weak_ptr<foo>`, as on [36]. When
+// `bar` needs to use `foo`, it checks if bar still exists by calling
+// `lock` on the `std::weak_ptr` to take temporary shared ownership
+// ([29]). If the returned `std::shared_ptr` is not empty, `bar` can
+// safely use it to access the `foo` object ([30-32]).
 
 int main()
 {
-	std::shared_ptr<foo> f = std::make_shared<foo>();
+	std::shared_ptr<bar> b = std::make_shared<bar>();
+	std::shared_ptr<foo> f = std::make_shared<foo>(b);
 
-	{
-		std::shared_ptr<bar> b = std::make_shared<bar>();
-		b->set_foo(f);
-		f->set_bar(b);
-	}
+	b->set_back_reference(f);
 
-	f->start();
+	b->do_something();
 }

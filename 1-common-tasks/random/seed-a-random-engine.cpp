af72ab43310e117c26e913567cf543be7393a8cf
Seth Cantrell <seth.cantrell@gmail.com>
2015-04-11 12:46:56 -0400

    fix line numbers

--- 1-common-tasks/random/seed-a-random-engine.cpp
+++ 1-common-tasks/random/seed-a-random-engine.cpp
@@ -13,13 +13,13 @@ int main() {
 // Seed a random number engine unpredictably
 //
 // In order to get unpredictable results from a pseudo-random number generator
-// we need a source of unpredictable seed data. On [6] we create a
+// we need a source of unpredictable seed data. On [7] we create a
 // [`std::random_device`](cpp/numeric/random/random_device) for this purpose. On
-// [7] we use a [`std::seed_seq`](cpp/numeric/random/seed_seq) to combine
+// [8] we use a [`std::seed_seq`](cpp/numeric/random/seed_seq) to combine
 // several values produced by `random_device` into a form suitable for seeding a
 // pseudo-random number generator. The more unpredictable data that is fed into
 // the `seed_seq`, the less predictable the results of the seeded engine will
-// be. On [9] we create a random number engine using the `seed_seq` to seed the
+// be. On [10] we create a random number engine using the `seed_seq` to seed the
 // engine's initial state.
 //
 // A `seed_seq` can be used to initialize multiple random number engines;

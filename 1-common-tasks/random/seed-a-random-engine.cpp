36d3eaa24910ae85ea6e5001897ff0259ab18c9b
Joseph Mansfield <sftrabbit@gmail.com>
2015-04-12 18:15:55 +0100

    Review/edit of high entropy seed sample

--- 1-common-tasks/random/seed-a-random-engine.cpp
+++ 1-common-tasks/random/seed-a-random-engine.cpp
@@ -1,4 +1,4 @@
-// Seeding A Random Number Engine Unpredictably
+// Seed with high entropy
 // C++11
 
 #include <random>
@@ -10,20 +10,23 @@ int main() {
 	std::mt19937 engine{seed_seq};
 }
 
-// Seed a random number engine unpredictably
+// Seed a random number engine with high entropy.
 //
-// In order to get unpredictable results from a pseudo-random number generator
-// we need a source of unpredictable seed data. On [7] we create a
-// [`std::random_device`](cpp/numeric/random/random_device) for this purpose. On
-// [8] we use a [`std::seed_seq`](cpp/numeric/random/seed_seq) to combine
-// several values produced by `random_device` into a form suitable for seeding a
-// pseudo-random number generator. The more unpredictable data that is fed into
-// the `seed_seq`, the less predictable the results of the seeded engine will
-// be. On [10] we create a random number engine using the `seed_seq` to seed the
-// engine's initial state.
+// To get unpredictable results from random number engines, we must
+// provide them with a source of entropy. To achieve greater quality
+// random number generation with higher entropy, we can provide a
+// random number engine with a sequence of seeds.
+// 
+// On [7], we create a [`std::random_device`](cpp/numeric/random/random_device)
+// as a source of non-deterministic random numbers (if such a source
+// is available). On [8], we initialise a [`std::seed_seq`](cpp/numeric/random/seed_seq)
+// with several initial seeds produced by the `std::random_device`. This
+// `std::seed_seq` generates a new sequence of seeds that are unbiased and
+// evenly distributed, even if the initial seeds were not. On [10], we
+// create a random number engine, seeding it with the `std::seed_seq`.
+// A particular `std::seed_seq` will produce the same sequence of
+// seeds each time it is used.
 //
-// A `seed_seq` can be used to initialize multiple random number engines;
-// `seed_seq` will produce the same seed data each time it is used.
-//
-// **Note**: Not all implemenations provide a source of non-deterministic data.
-// Check your implementation's documentation for `std::random_device`.
+// A `std::seed_seq` can also be used to initialize a large number of
+// random number engines, providing each with a random seed generated
+// by the `std::seed_seq`.

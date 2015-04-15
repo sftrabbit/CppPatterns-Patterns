8a9f2c8e7563acfb33d5fbcf4fa3a8799f539758
Joseph Mansfield <sftrabbit@gmail.com>
2015-04-15 21:45:57 +0100

    Review/edit swap containers sample

--- 1-common-tasks/algorithms/swap-containers.cpp
+++ 1-common-tasks/algorithms/swap-containers.cpp
@@ -1,4 +1,4 @@
-// Swap the contents of two containers
+// Swap containers
 
 #include <algorithm>
 #include <utility>
@@ -19,42 +19,23 @@ int main()
 
 // Swap the contents of two containers.
 // 
-// On [9-10] we create a pair of `vectors`s, and fill them with values.
+// On [9-10], we create two [`std::vector`](cpp/container/vector)s,
+// and fill them with values.
 // 
-// On [12] we swap the contents of the two vectors using
-// [`std::swap_ranges`](cpp/algorithm/swap_ranges), which goes through
-// two ranges and does an element-by-element swap of their values.
-// (So in this example, it does four swaps - one for each of the four
-// elements in the `vector`s.)
+// On [12], we swap the contents of the two `std::vector`s using the
+// [`std::swap_ranges`](cpp/algorithm/swap_ranges) algorithm, which
+// is given the beginning and end iterators of one range and the 
+// beginning iterator of a second range and performs an
+// element-by-element swap of their values (in this case, four swaps).
+// The second range must have at least the number of elements in the
+// first range. After this operation, iterators remain valid and point
+// at the same container as they did previously.
 // 
-// On [14] we swap the `vector`s' contents all in one shot - not
-// element-by-element.
-// (`v2.swap(v1)` would have the same effect.)
-// 
-// On [16-17] we again swap the `vector`s all in one shot, this time
-// using the generic swap pattern.
-// 
-// The difference between `swap_ranges()` and the `swap()` methods is
-// that `swap_ranges()` goes through both containers element-by-element
-// and swaps each pair of elements individually, while `swap()` (either
-// the member function or the generic non-member function) swaps the
-// *entire* container at once.
-// The two methods each have their pros and cons.
-// 
-// `swap_ranges()` is generally much slower than `swap()`, because it
-// has to swap each pair of values one at a time, rather than all at
-// once.
-// It also requires that both of the ranges we swap are the same size.
-// However, it does not invalidate the iterators of the container, and
-// it allows us to swap *portions* of a container - even within
-// the same container (for example, if your container has 10 or more
-// elements we could swap the first 5 elements with the last 5).
-// 
-// `swap()` is usually *much* faster than `swap_ranges()` - for large
-// containers it can be thousands or millions of times faster (but
-// this is not true for *all* container types).
-// `swap()` can swap the contents of two containers even if they are
-// not the same size (so we can swap a container with 100 elements
-// with an empty container).
-// However, it will invalidate the container's iterators, and there is
-// no option to swap only portions of a container with `swap()`.
+// On [14], we use `std::vector`'s `swap` member function to swap the
+// contents all in one shot. [!16-17] perform the same operation with
+// the [generic swap pattern](/common-tasks/algorithms/swap-values.html).
+// This approach is much faster than `std::swap_ranges`, as it simply
+// swaps over the internal storage in constant time. It also does not
+// require the containers to have the same size. However, after the
+// operation, iterators will now point at the other container and
+// past-the-end iterators are invalidated.

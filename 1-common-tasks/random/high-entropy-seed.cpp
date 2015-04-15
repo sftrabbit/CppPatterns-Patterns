6810784b41d19e9940ba38064afb06e73150dbff
Joseph Mansfield <sftrabbit@gmail.com>
2015-04-15 20:53:11 +0100

    Revision of unpredictable seeding sample

--- 1-common-tasks/random/high-entropy-seed.cpp
+++ 1-common-tasks/random/high-entropy-seed.cpp
@@ -1,4 +1,4 @@
-// High entropy seeds
+// Unpredictable random numbers
 // C++11
 
 #include <random>
@@ -10,23 +10,25 @@ int main() {
 	std::mt19937 engine{seed_seq};
 }
 
-// Seed a random number engine with high entropy.
+// Seed a random number engine with greater unpredictability.
 //
-// To get unpredictable results from random number engines, we must
-// provide them with a source of entropy. To achieve greater quality
-// random number generation with higher entropy, we can provide a
-// random number engine with a sequence of seeds.
+// A pseudo-random number engine is initialised with a seed, which
+// determines the initial state of the engine. By providing such
+// an engine with a greater amount of unpredictable seed data, we can
+// achieve higher quality random number generation.
 // 
 // On [7], we create a [`std::random_device`](cpp/numeric/random/random_device)
 // as a source of non-deterministic random numbers (if such a source
 // is available). On [8], we initialise a [`std::seed_seq`](cpp/numeric/random/seed_seq)
-// with several initial seeds produced by the `std::random_device`. This
-// `std::seed_seq` generates a new sequence of seeds that are unbiased and
-// evenly distributed, even if the initial seeds were not. On [10], we
-// create a random number engine, seeding it with the `std::seed_seq`.
-// A particular `std::seed_seq` will produce the same sequence of
-// seeds each time it is used.
-//
-// A `std::seed_seq` can also be used to initialize a large number of
-// random number engines, providing each with a random seed generated
-// by the `std::seed_seq`.
+// with several initial seeds produced by the `std::random_device`.
+// On [10], we create a random number engine, seeding it with this
+// `std::seed_seq`. This provides the random number engine with
+// a new sequence of seeds that are unbiased and evenly distributed,
+// even if the initial seeds were not. Providing more initial seeds
+// increases the unpredictability of the engine.
+// 
+// While a `std::seed_seq` can be used to initialize multiple random
+// number engines, bear in mind that a particular `std::seed_seq` will
+// produce the same sequence of seeds each time it is used. We may,
+// however, use a `std::seed_seq` to generate individual evenly
+// distributed seeds for a large number of random number engines.

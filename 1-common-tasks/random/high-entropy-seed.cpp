7fa243fe17c53e5180d0dbdf7907527440d012a9
Joseph Mansfield <sftrabbit@gmail.com>
2015-04-18 17:50:11 +0100

    Edit unpredictable random numbers sample

--- 1-common-tasks/random/high-entropy-seed.cpp
+++ 1-common-tasks/random/high-entropy-seed.cpp
@@ -13,22 +13,26 @@ int main() {
 // Seed a random number engine with greater unpredictability.
 //
 // A pseudo-random number engine is initialised with a seed, which
-// determines the initial state of the engine. By providing such
-// an engine with a greater amount of unpredictable seed data, we can
-// achieve higher quality random number generation.
+// determines the initial state of the engine. When seeding with
+// a single 32 bit value (as is typically provided by
+// [std::random_device](cpp/numeric/random/random_device)), the
+// engine can be in one of only 2<sup>32</sup> states. By providing an
+// an engine with a greater amount of random seed data, we increase
+// the number of possible states and therefore the unpredicitability
+// of the engine.
 // 
-// On [7], we create a [`std::random_device`](cpp/numeric/random/random_device)
-// as a source of non-deterministic random numbers (if such a source
-// is available). On [8], we initialise a [`std::seed_seq`](cpp/numeric/random/seed_seq)
+// On [7], we create a `std::random_device` as a source of
+// non-deterministic random numbers (if such a source is available).
+// On [8], we initialise a [`std::seed_seq`](cpp/numeric/random/seed_seq)
 // with several initial seeds produced by the `std::random_device`.
-// On [10], we create a random number engine, seeding it with this
-// `std::seed_seq`. This provides the random number engine with
-// a new sequence of seeds that are unbiased and evenly distributed,
-// even if the initial seeds were not. Providing more initial seeds
-// increases the unpredictability of the engine.
+// On [10], we seed a random number engine with this `std::seed_seq`,
+// which provides the engine with a sequence of evenly
+// distributed seeds with low bias (although [not
+// bias-free](http://www.pcg-random.org/posts/cpp-seeding-surprises.html)).
+// Providing more initial seeds will increase the unpredictability of
+// the engine by increasing the number of possible seed sequences that
+// might be generated.
 // 
 // While a `std::seed_seq` can be used to initialize multiple random
 // number engines, bear in mind that a particular `std::seed_seq` will
-// produce the same sequence of seeds each time it is used. We may,
-// however, use a `std::seed_seq` to generate individual evenly
-// distributed seeds for a large number of random number engines.
+// produce the same sequence of seeds each time it is used.

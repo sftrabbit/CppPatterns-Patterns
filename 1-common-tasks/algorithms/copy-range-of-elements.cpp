c9ef433b8a62be98699dabbb7b4f2e6c61037fbb
Joseph Mansfield <sftrabbit@gmail.com>
2015-09-25 20:43:03 +0100

    Update copy range sample

--- 1-common-tasks/algorithms/copy-range-of-elements.cpp
+++ 1-common-tasks/algorithms/copy-range-of-elements.cpp
@@ -5,38 +5,50 @@
 #include <algorithm>
 #include <iterator>
 
-int main()
+std::vector<int> target2(5);
+std::vector<int> target3;
+
+template <typename RangeOfInts>
+void foo(RangeOfInts source)
 {
-	std::vector<int> source = {1, 2, 3, 4, 5};
-
-	std::vector<int> target1(5);
-
-	std::copy(std::begin(source), std::end(source),
-	          std::begin(target1));
-
-	std::vector<int> target2;
+	std::vector<int> target1{std::begin(source),
+	                         std::end(source)};
 
 	std::copy(std::begin(source), std::end(source),
-	          std::back_inserter(target2));
+	          std::begin(target2));
+
+	std::copy(std::begin(source), std::end(source),
+	          std::back_inserter(target3));
 }
 
-// Copy elements from one range to another.
+// Copy elements from a range to another range or container.
 // 
-// On [10], we create a [`std::vector<int>`](cpp/container/vector)
-// called `source` containing a sequence of values that we wish to copy. 
+// We start with a `source` range object on [12] and, for the purposes
+// of this example, we assume its elements are of type `int`.
 // 
-// On [12], we have another `std::vector<int>`, `target1`, which
-// is initialised with 5 elements. We then use
-// [`std::copy`](cpp/algorithm/copy) on [14-15] to copy values from
-// `source` to `target1`. The first two iterator arguments denote the source
-// range, and the third iterator argument denotes the target range.
-// We use [`std::begin`](cpp/iterator/begin) and
-// [`std::end`](cpp/iterator/end) to get these iterators for
-// `source` and `target1`.
+// On [14-15], we copy the elements from the `source` range into a
+// container, `target1`, simply by passing the begin and end iterators
+// of the range to the `std::vector<T>`'s constructor. We use
+// [`std::begin`](cpp/iterator/begin) and [`std::end`](cpp/iterator/end)
+// to obtain these iterators.
+//
+// To copy the elements of `source` into a range or container which
+// already has the appropriate number of elements allocated,
+// represented by `target2` on [8], we use [`std::copy`](cpp/algorithm/copy)
+// on [17-18]. The first two iterator arguments denote the source
+// range, and the third iterator argument denotes the start of the
+// target range. For this to work, the elements must already exist in
+// the target range.
 //
 // To demonstrate how we can copy into a container that does not
-// yet contain any elements, we create an empty `std::vector<int>`
-// called `target2` on [17]. For the third argument of
-// `std::copy` ([19-20]), we call [`std::back_inserter`](cpp/iterator/back_inserter)
-// to get an iterator that automatically calls `push_back` on `target2`
+// yet contain any elements, we have an empty `std::vector<int>`
+// called `target3` on [9]. For the third argument of
+// `std::copy` ([20-21]), we call [`std::back_inserter`](cpp/iterator/back_inserter)
+// to get an iterator that automatically calls `push_back` on `target3`
 // for each element that is copied.
+
+int main()
+{
+	std::vector<int> vec = {5, 4, 3, 2, 1};
+	foo(vec);
+}

8b19a2543ea835d1aef2643b9b086f6ea4b8ec21
Joseph Mansfield <sftrabbit@gmail.com>
2015-09-26 14:53:09 +0100

    Review/edit erase-remove sample

--- 1-common-tasks/containers/erase-remove-idiom.cpp
+++ 1-common-tasks/containers/erase-remove-idiom.cpp
@@ -1,4 +1,4 @@
-// Erase-Remove idiom
+// Remove elements from a container
 // C++11
 
 #include <vector>
@@ -6,30 +6,39 @@
 
 int main()
 {
-	std::vector<int> v {1,2,3,4,2,5,2,6};
-	auto it = std::remove(std::begin(v), std::end(v), 2);
-	v.erase(it, std::end(v)); 
-	// now v is {1,3,4,5,6}	
+	std::vector<int> v = {1, 2, 3, 4, 2, 5, 2, 6};
+
+	v.erase(std::remove(std::begin(v), std::end(v), 2),
+	        std::end(v)); 
 	
-	// remove all even numebers
-	v.erase( std::remove_if( std::begin(v), std::end(v), [](int i) { return i%2 == 0; } ), std::end(v) ); 
-	// now v is {1,3,5}
+	v.erase(std::remove_if(std::begin(v), std::end(v),
+	                       [](int i) { return i%2 == 0; }),
+	        std::end(v)); 
 }
 
-// Remove elements that satisfy a certain condition from a standard container.
+// Use the erase-remove idiom to remove elements from a container.
 // 
 // On [9], we create a [`std::vector`](cpp/container/vector) as an example
-// container and initialize it with int values.
-// 
-// On [10], we "remove" all the occurrences of the element 2,
-// using the algorithm [`std::remove`](cpp/algorithm/remove) which actually shifts 
-// (by means of move assignment) the elements in the range in such a way that 
-// the elements that are not to be removed appear in the beginning of the range.
-// This algorithm returns a past-the-end iterator for the new end of the range.
+// container and initialize it some `int` elements.
 //
-// On [11] we apply the second part of the idiom, by using vector's member function
-// [`erase`](cpp/container/vector/erase) which really removes the elements in the range.
-// 
-// Generally this idiom is applied on a single line, as shown on line [15]. Here in particular,
-// we used [`std::remove_if`](cpp/algorithm/remove) which "removes" all elements for which 
-// a provided predicate returns true. We simply removed all the even values.
+// The [`std::remove` and `std::remove_if`](cpp/algorithm/remove)
+// algorithms do not have knowledge about the underlying storage of
+// the given range, so cannot actually remove elements from that
+// storage. Instead, these algorithms actually shift (by means of move
+// assignment) the elements in the range in such a way that the
+// elements not removed form a new range at the beginning of the
+// original range. The algorithms return a past-the-end iterator for
+// this new range, which, since the removed elements have been shifted
+// to the end of the original range, also marks the start of the
+// removed elements.
+//
+// On [11-12], we show how we can pass the resulting iterator to the
+// container's [`erase`](cpp/container/vector/erase) member function,
+// which actually removes the elements from the container. Similarly,
+// on [14-16], we demonstrate how `std::remove_if` can be used with
+// `erase` to remove all elements for which a given predicate returns
+// `true` (in the sample, we remove all even elements).
+//
+// This technique of using the generic remove algorithms followed by
+// a call to the particular container's `erase` member function is
+// commonly referred to as the erase-remove idiom.

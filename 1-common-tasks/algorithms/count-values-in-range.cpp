6d6f1929d25ed9845a1fa8af544a0703175ceeea
Joseph Mansfield <sftrabbit@gmail.com>
2016-02-08 21:30:14 +0000

    Tidy up std::count sample

--- 1-common-tasks/algorithms/count-values-in-range.cpp
+++ 1-common-tasks/algorithms/count-values-in-range.cpp
@@ -1,5 +1,4 @@
-//Counting occurrence of particular element in a Range of elements
-//C++11
+// Count occurrences of value in a range
 
 # include <iostream>
 # include <algorithm>
@@ -7,18 +6,25 @@
 
 int main()
 {
- std::vector<int> numbers = {1,2,3,5,6,3,4,1};
- int element_to_look_for = 3;
- std::cout<<"Number of times 3 occurs : "<<std::count(std::begin(numbers),std::end(numbers),element_to_look_for);
+	std::vector<int> numbers = {1, 2, 3, 5, 6, 3, 4, 1};
+
+	int count = std::count(std::begin(numbers),
+	                       std::end(numbers),
+	                       3);
 }
 
-//Count occurrences of a particular element in a range of elements
+// Count the number of occurrences of a particular value in a range of
+// elements.
 //
-// On [10] we create a [`std::vector`](cpp/container/vector) of `int` to store elements.
+// On [9], we create a [`std::vector`](cpp/container/vector) of `int`
+// initialized with some values.
 //
-// On [12], we call the standard alrogithm
-// [`std::count`](cpp/algorithm/count), which counts the occurrence of a particular element, (which in this
-// case is 3, stored in the variable named `element_to_look_for` on [11]).
-// We use [`std::begin`](cpp/iterator/begin) and
-// [`std::end`](cpp/iterator/end) to get the begin and end iterators to specify range of the container
-// To count elements which meet a certain condition, use [`std::count`](cpp/algorithm/count).
+// On [11-13], we use the alrogithm [`std::count`](cpp/algorithm/count),
+// to count the occurrences of a particular value in the `std::vector`.
+// For the first two arguments on [11-12], we use [`std::begin`](cpp/iterator/begin)
+// and [`std::end`](cpp/iterator/end) to get the begin and end
+// iterators for the range in which we wish to count. The third
+// argument on [13] is the value to count the occurrences of.
+//
+// To count elements according to a predicate, you can use
+// [`std::count_if`](cpp/algorithm/count) instead.

47a5876ee143d3950e80b2ec2bd8260d9a651028
Mark A. Gibbs <indi.in.the.wired@gmail.com>
2016-02-18 17:28:26 -0500

    Refactored sample code as separate function

--- 1-common-tasks/input-streams/read-text-file.cpp
+++ 1-common-tasks/input-streams/read-text-file.cpp
@@ -4,17 +4,14 @@
 #include <sstream>
 #include <string>
 
-int main()
+std::string read_text_file(std::string const& filename)
 {
-	std::ifstream file{"file.txt"};
-	if (file)
-	{
-		std::ostringstream ss{};
-		if (ss << file.rdbuf())
-		{
-			std::string str = ss.str();
-		}
-	}
+	std::ifstream file{filename};
+	
+	std::ostringstream ss{};
+	ss << file.rdbuf();
+	
+	return ss.str();
 }
 
 // Read an entire text file into a string.
@@ -23,20 +20,25 @@ int main()
 // [item-by-item](/common-tasks/read-line-of-values.html) or
 // [line-by-line](/common-tasks/read-line-by-line.html). But sometimes
 // we want to read an entire text file into a string variable, in one
-// shot and without any formatting.
+// shot, without any parsing or formatting.
 // 
-// On [9], we create an input stream for the file `file.txt`, and
-// confirm the file was opened successfully on [10].
+// On [9], we create an input stream for the file that we will be
+// reading.
 // 
 // We create a [`std::ostringstream`](cpp/io/basic_ostringstream) on
-// [12]; this is an output stream because we are reading *in* from the
+// [11]; this is an output stream because we are reading *in* from the
 // file stream, and writing what we read *out* to the string stream. We
-// do this on [13], using file stream's
+// do this on [12], using file stream's
 // [`rdbuf()`](cpp/io/basic_ifstream/rdbuf) function to get a pointer to
 // its internal buffer. (We could also have used a
 // [`std::filebuf`](cpp/io/basic_filebuf) object directly, without
 // a `std::ifstream`.)
 // 
 // If we were able to successfully read the file's contents, we can
-// recover them as a string variable using `std::ostringstream`'s
-// [`str()`](cpp/io/basic_ostringstream/str) function, as on [15].
+// recover them as a string using `std::ostringstream`'s
+// [`str()`](cpp/io/basic_ostringstream/str) function, as on [14].
+
+int main()
+{
+	std::string str = read_text_file("file.txt");
+}

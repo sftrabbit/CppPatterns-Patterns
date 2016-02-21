859b68ce2506c9d294a458c2d0e7cb93fe2a8ec6
Mark A. Gibbs <indi.in.the.wired@gmail.com>
2016-02-20 21:06:39 -0500

    Converted text input from 'file' to 'stream'

--- 1-common-tasks/input-streams/read-text-stream.cpp
+++ 1-common-tasks/input-streams/read-text-stream.cpp
@@ -1,44 +1,41 @@
-// Read text file
+// Read text stream
 
 #include <fstream>
 #include <sstream>
 #include <string>
 
-std::string read_text_file(std::string const& filename)
+std::string read_text_stream(std::istream& in)
 {
-	std::ifstream file{filename};
-	
 	std::ostringstream ss{};
-	ss << file.rdbuf();
+	ss << in.rdbuf();
 	
 	return ss.str();
 }
 
-// Read an entire text file into a string.
+// Read entire content of a text stream into a string.
 // 
-// Sometimes we want to read data from a file
+// Sometimes we want to read data from an input stream
 // [item-by-item](/common-tasks/read-line-of-values.html) or
 // [line-by-line](/common-tasks/read-line-by-line.html). But sometimes
-// we want to read an entire text file into a string variable, in one
-// shot, without any parsing or formatting.
+// we want to read the entire stream's text content into a string
+// variable, in one shot, without any parsing or formatting.
 // 
-// On [9], we create an input stream for the file that we will be
-// reading.
+// On [9] we create a
+// [`std::ostringstream`](cpp/io/basic_ostringstream). This is an output
+// stream because we are reading *in* from the input stream (`in`), and
+// writing what we read *out* to the string stream. We do this on [10],
+// using input stream's  [`rdbuf()`](cpp/io/basic_ifstream/rdbuf)
+// function to get a pointer to its internal buffer. (We could also have
+// used a [`std::streambuf`](cpp/io/basic_streambuf) object directly,
+// without requiring a `std::istream` wrapper.)
 // 
-// We create a [`std::ostringstream`](cpp/io/basic_ostringstream) on
-// [11]; this is an output stream because we are reading *in* from the
-// file stream, and writing what we read *out* to the string stream. We
-// do this on [12], using file stream's
-// [`rdbuf()`](cpp/io/basic_ifstream/rdbuf) function to get a pointer to
-// its internal buffer. (We could also have used a
-// [`std::filebuf`](cpp/io/basic_filebuf) object directly, without
-// a `std::ifstream`.)
-// 
-// If we were able to successfully read the file's contents, we can
+// If we were able to successfully read the stream's contents, we can
 // recover them as a string using `std::ostringstream`'s
-// [`str()`](cpp/io/basic_ostringstream/str) function, as on [14].
+// [`str()`](cpp/io/basic_ostringstream/str) function, as on [12].
 
 int main()
 {
-	std::string str = read_text_file("file.txt");
+	std::ifstream in{"file.txt"};
+	
+	std::string str = read_text_stream(in);
 }

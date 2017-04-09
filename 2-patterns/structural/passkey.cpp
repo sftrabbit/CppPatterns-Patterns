ee7d7d30e3a63d6953bb75633ee84f1b56a0ceaa
Tommy Nguyen <remyabel@gmail.com>
2017-04-09 03:10:27 -0400

    Remove instances of 'you'

--- 2-patterns/structural/passkey.cpp
+++ 2-patterns/structural/passkey.cpp
@@ -29,12 +29,12 @@ class baz {
 // Allow granular access to a class' constructors and its methods by
 // requiring friendship of a passkey.
 //
-// Friendship in C++ is all-or-nothing; you cannot share only some
-// methods with friendship. The [Passkey
-// idiom](https://arne-mertz.de/2016/10/passkey-idiom/) works around that by
-// "tagging" exposed methods with a `Key` parameter. If a caller
-// class such as `baz` wants to call `foo`'s constructor or methods,
-// it must be a friend of `Key`.
+// Friendship in C++ is all-or-nothing; classes cannot befriend only
+// some parts of another class. The [Passkey
+// idiom](https://arne-mertz.de/2016/10/passkey-idiom/) works around
+// that by "tagging" exposed methods with a `Key` parameter. If a
+// caller class such as `baz` wants to call `foo`'s constructor or
+// methods, it must be a friend of `Key`.
 //
 // The `Key` class defined on [5-8] does not need to be private or
 // even defined within `foo`. To disallow `Key` from being created via
@@ -47,5 +47,5 @@ class baz {
 // the Passkey idiom preserves encapsulation.
 //
 // [23-25] show how the caller would construct `foo` and call its
-// member function `qux`. You simply pass a temporary object of type
+// member function `qux`. We simply pass a temporary object of type
 // `Key` as a parameter using C++11's brace syntax.

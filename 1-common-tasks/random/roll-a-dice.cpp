37b09883a6d1f478eb8addf8f12ab6446b942400
Cassiano Montanari <cassianomontanari@gmail.com>
2017-09-15 14:42:53 -0300

    Update and rename roll-a-die.cpp to roll-a-dice.cpp

--- 1-common-tasks/random/roll-a-dice.cpp
+++ 1-common-tasks/random/roll-a-dice.cpp
@@ -1,4 +1,4 @@
-// Roll a die
+// Roll a dice
 // C++11
 
 #include <random>
@@ -7,9 +7,9 @@ int main()
 {
 	std::random_device random_device;
 	std::mt19937 random_engine{random_device()};
-	std::uniform_int_distribution<int> die_distribution{1, 6};
+	std::uniform_int_distribution<int> dice_distribution{1, 6};
 	
-	int die_roll = die_distribution(random_engine);
+	int dice_roll = dice_distribution(random_engine);
 }
 
 // Generate a random integer according to a uniform distribution.
@@ -32,7 +32,7 @@ int main()
 // Finally, we generate a random number from the random engine,
 // distributed according to the uniform integer distribution ([12]).
 // This gives us a random integer from 1 to 6, as though we had rolled
-// a 6-sided die.
+// a 6-sided dice.
 // 
 // To generate further random numbers, simply call
-// `die_distribution(random_engine)` again.
+// `dice_distribution(random_engine)` again.
